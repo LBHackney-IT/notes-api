e5217d5a1ed17e495634c38969b5d853bb531640
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-05-26 16:31:47 +0100

    Integrated NuGet package. Implemented DynamoDb HealthCheck

---
+++ NotesApi.Tests/V1/E2ETests/Steps/HealthCheckSteps.cs
@@ -0,0 +1,46 @@
+using FluentAssertions;
+using Hackney.Core.HealthCheck;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace NotesApi.Tests.V1.E2ETests.Steps
+{
+    public class HealthCheckSteps : BaseSteps
+    {
+        public HealthCheckSteps(HttpClient httpClient) : base(httpClient)
+        { }
+
+        private async Task<HttpResponseMessage> CallApi()
+        {
+            var route = $"api/v1/healthcheck/ping";
+            var uri = new Uri(route, UriKind.Relative);
+            return await _httpClient.GetAsync(uri).ConfigureAwait(false);
+        }
+
+        private async Task<HealthCheckResponse> ExtractResultFromHttpResponse(HttpResponseMessage response)
+        {
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var apiResult = JsonSerializer.Deserialize<HealthCheckResponse>(responseContent, _jsonOptions);
+            return apiResult;
+        }
+
+        public async Task WhenTheHealtchCheckIsCalled()
+        {
+            _lastResponse = await CallApi().ConfigureAwait(false);
+        }
+
+        public async Task ThenTheHealthyStatusIsReturned()
+        {
+            var apiResult = await ExtractResultFromHttpResponse(_lastResponse).ConfigureAwait(false);
+            apiResult.Status.Should().Be(HealthStatus.Healthy);
+            apiResult.Entries.First().Key.Should().Be("DynamoDb");
+            apiResult.Entries["DynamoDb"].Status.Should().Be(HealthStatus.Healthy);
+        }
+    }
+}
---
+++ NotesApi.Tests/V1/E2ETests/Stories/HealthCheckTests.cs
@@ -0,0 +1,49 @@
+using NotesApi.Tests.V1.E2ETests.Steps;
+using System;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace NotesApi.Tests.V1.E2ETests.Stories
+{
+    [Story(
+        AsA = "Api client",
+        IWant = "to be able to validate that the service satus is healty",
+        SoThat = "I can be sure that calls made to it will succeed.")]
+    [Collection("DynamoDb collection")]
+    public class HealthCheckTests : IDisposable
+    {
+        private readonly DynamoDbIntegrationTests<Startup> _dbFixture;
+        private readonly HealthCheckSteps _steps;
+
+        public HealthCheckTests(DynamoDbIntegrationTests<Startup> dbFixture)
+        {
+            _dbFixture = dbFixture;
+            _steps = new HealthCheckSteps(_dbFixture.Client);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
+            }
+        }
+
+        [Fact]
+        public void ServiceReturnsHealthyStatus()
+        {
+            this.Given("A running service")
+                .When(w => _steps.WhenTheHealtchCheckIsCalled())
+                .Then(t => _steps.ThenTheHealthyStatusIsReturned())
+                .BDDfy();
+        }
+    }
+}
