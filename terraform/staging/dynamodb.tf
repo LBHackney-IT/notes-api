ee5751de4bcda7d3503261ce4957ee702f51c9e2
humulla <humairaa.mulla@hackney.gov.uk>
2021-07-28 11:22:00 +0100

    Backup tags for Dynamodb

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -32,6 +32,7 @@ resource "aws_dynamodb_table" "notesapi_dynamodb_table" {
         Environment       = var.environment_name
         terraform-managed = true
         project_name      = var.project_name
+        backup_policy     = "Stg"
     }
     
     point_in_time_recovery {
