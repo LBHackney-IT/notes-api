b99cce55ec58e879727edeb6cd5c9c9a59ebf9f2
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-08-03 11:59:44 +0100

    Release mttl 1101 (#40)
    * MMH team has been added as code owners
    
    * Removed Id from AuthorDetails (#38)
    
    * Updated Hackney.Core package version (#39)
    
    Co-authored-by: humulla <humairaa.mulla@hackney.gov.uk>
    Co-authored-by: humulla <43747248+humulla@users.noreply.github.com>

--- NotesApi/V1/Domain/AuthorDetails.cs
+++ NotesApi/V1/Domain/AuthorDetails.cs
@@ -2,8 +2,6 @@ namespace NotesApi.V1.Domain
 {
     public class AuthorDetails
     {
-        public string Id { get; set; }
-
         public string FullName { get; set; }
 
         public string Email { get; set; }
