44ac9cbdfd0f21072e0d88f38e3008d7fd4d86c3
LBHTKarki <tuomo.karki@hackney.gov.uk>
2025-05-20 09:39:02 +0100

    add pre-production resources and workflows

---
+++ terraform/pre-production/dynamodb.tf
@@ -0,0 +1,38 @@
+resource "aws_dynamodb_table" "notesapi_dynamodb_table" {
+  name           = "Notes"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "targetId"
+  range_key      = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "targetId"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+
+  local_secondary_index {
+    name            = "NotesByCreated"
+    range_key       = "createdAt"
+    projection_type = "ALL"
+  }
+
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Dev", Backup = false, Confidentiality = "Internal" }
+  )
+
+  point_in_time_recovery {
+    enabled = false
+  }
+}
